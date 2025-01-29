a420d92877451dfddf7c74ea5c7ac7c0dda3dba2
Naman Adhikari <adhikarinaman00@gmail.com>
2025-01-29 19:16:50 +0545

    nixos config

--- configuration.nix
+++ configuration.nix
@@ -1,30 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
   networking.networkmanager.enable = true;
   
-  #enable pass secret
  services.passSecretService.enable = true;
  
   
@@ -43,17 +33,13 @@ nix.settings.experimental-features = [ "nix-command" "flakes"];
 
 
   environment.sessionVariables = {
-  #If your cursor becomes invisible
   # WLR_NO_HARDWARE_CURSORS = "1";
-  #Hint electron apps to use wayland
    NIXOS_OZONE_WL = "1";
  };
 
   hardware = {
-   # Opengl
     opengl.enable = true;
 
-   #Most wayland compositors need this
     nvidia.modesetting.enable = true;
 };
 
@@ -72,10 +58,8 @@ services.tlp.settings = {
     STOP_CHARGE_THRESH_BAT0 = 80;
 };
 
-  # Set your time zone.
   time.timeZone = "Asia/Kathmandu";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -90,29 +74,25 @@ services.tlp.settings = {
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-  # Enable Bluetooth
-  hardware.bluetooth.enable = true;
-  # Enable CUPS to print documents.
+
+
+hardware.bluetooth.enable = true;
   services.printing.enable = true;
   
 
   services.power-profiles-daemon.enable = false;
 
 
-  # Enable sound with pipewire.
+
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -120,18 +100,11 @@ services.tlp.settings = {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-    #services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.lostfromlight = {
     isNormalUser = true;
     description = "Naman Adhikari";
@@ -142,14 +115,10 @@ services.tlp.settings = {
     ];
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     wget
     waybar
@@ -188,44 +157,9 @@ services.tlp.settings = {
     hypridle
  ];
 
-# hardware.opengl = {
- #   enable = true;
-  #  driSupport32Bit = true;
-   # extraPackages = with pkgs; [
-    #    intel-compute-runtime
-   # ];
- # };
 
 
 
 
 
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  #system.stateVersion = "24.05"; # Did you read the comment?
-
-}
