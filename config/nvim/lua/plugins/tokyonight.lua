fc70dc306ef59b9e3dbaf488652e5f2b9b65dcb4
Naman Adhikari <adhikarinaman00@gmail.com>
2025-02-20 20:23:56 +0545

    neovim with more plugins

--- config/nvim/lua/plugins/tokyonight.lua
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -1,6 +1,31 @@
-return {
+--[[return {
+    "EdenEast/nightfox.nvim", lazy = false, priority = 1000,
+	config= function()
+vim.cmd.colorscheme "carbonfox"
+end
+}]]-- 
+
+
+--[[return {
+    "rebelot/kanagawa.nvim", lazy = false, priority = 1000,
+	config= function()
+vim.cmd.colorscheme "kanagawa-dragon"
+end
+}]]--
+
+return{
+  "scottmckendry/cyberdream.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme cyberdream")
+  end,
+}
+
+--[[return {
     "folke/tokyonight.nvim", lazy = false, priority = 1000,
 	config= function()
 vim.cmd.colorscheme "tokyonight-night" 
 end
-}	
+}]]--	
+
