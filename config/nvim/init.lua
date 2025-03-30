735d30944d4c8b9a788a7aff1dd4cff765139637
Naman-Adhikari <adhikarinaman00@gmail.com>
2025-03-30 18:25:56 +0545

    shaders and folding

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,22 +1,38 @@
- require "options"
+-- Load basic options first
+require "options"
 
- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then  -- for lazy package manager
+-- Lazy.nvim setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
-
-
+-- Initialize plugins
 require("lazy").setup("plugins")
 
 
+-- Fold settings (MUST come before plugin setup)
+vim.opt.foldmethod = "expr"               -- Use Treesitter for folding
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevelstart = 99               -- Start with all folds open
+--vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"  -- Nicer fold display
+vim.opt.foldtext = [[v:folddashes.substitute(getline(v:foldstart),'\t',repeat(' ',&tabstop),'g')]]
+vim.cmd([[
+  highlight Folded guifg=#87cde0 guibg=#454747 gui=italic
+]])
+
+-- Optional: Better fold characters (requires NerdFont)
+vim.opt.fillchars = {
+  fold = " ",
+  foldopen = "",  -- Down arrow icon
+  foldclose = "", -- Right arrow icon
+}
 
