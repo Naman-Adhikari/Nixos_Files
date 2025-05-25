3127eea1bbc1079e6874d19e416a0648e23ce2ab
Naman-Adhikari <adhikarinaman00@gmail.com>
2025-05-25 19:27:24 +0545

    Compiler.nvim added

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -1,55 +1,63 @@
 return {
   {
     "williamboman/mason.nvim",
+	commit = "00f14f787c71d060db9fa9b0db44c6cf12b64f61",	
+    lazy = false,
+    priority = 1000,
     config = function()
       require("mason").setup()
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pylsp", "vtsls", "html", "cssls" }
-      })
-    end,
+	commit = "1a634d623f16bba0f2f5746c75882d28f47a9c7f",	
+    dependencies = { "williamboman/mason.nvim" },
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp", -- ensure this is installed too
+    },
     config = function()
-	  local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
-      local on_attach = function(client, bufnr)
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local on_attach = function(_, bufnr)
         local opts = { buffer = bufnr, noremap = true, silent = true }
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
       end
 
-      -- Setup for Lua LSP
-      lspconfig.lua_ls.setup({
-				on_attach = on_attach,
-				capabilities = capabilities
-			})
-
-		lspconfig.vtsls.setup({})	   
-		lspconfig.html.setup({})	   
-		lspconfig.cssls.setup({})	   
-
-		 
-
-      -- Setup for Python LSP with trailing whitespace warnings disabled
-      lspconfig.pylsp.setup({
-        on_attach = on_attach,
-        settings = {
-          pylsp = {
-            plugins = {
-              pycodestyle = {
-                ignore = { "E265", "E231", "E225", "E261", "W293", "E302", "E303", "E222", "E262", "E305", "W291", "E501","W391", "E202", "E225", "E114", "E201" }, -- Ignore trailing whitespace warnings
+      -- LSP setups
+      local servers = {
+        lua_ls = {},
+        vtsls = {},
+        html = {},
+        cssls = {},
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  ignore = {
+                    "E265", "E231", "E225", "E261", "W293", "E302",
+                    "E303", "E222", "E262", "E305", "W291", "E501",
+                    "W391", "E202", "E114", "E201"
+                  },
+                },
               },
             },
-          },
+          }
         },
-      })
+      }
+
+      for server, config in pairs(servers) do
+        config.on_attach = on_attach
+        config.capabilities = capabilities
+        lspconfig[server].setup(config)
+      end
     end,
   },
 }
